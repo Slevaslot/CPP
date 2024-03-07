253dfae1c72ff584d1a13798ed21f24e6d351ca9
Albert PROUST <aproust@bess-f4r4s10.clusters.42paris.fr>
2024-03-07 16:44:19 +0100

    cpp09

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -10,28 +10,28 @@ int main (int ac, char **argv) {
 		std::map<std::string, float> map;
 		std::ifstream file(argv[1]);
 		std::string line;
-		while (getline(file, line)) {
+		while (getline(file, line))
+		{
 			size_t i = 0;
-			line.find("|", i);
+			i = line.find("|", i);
 			if (i != std::string::npos) {
-				std::string key;
 				std::stringstream fl;
-				for (size_t j = 0; j < i; j++) {
-					key[j] = line[j];
-				}
-				i++;
-				key[i] = '\0';
 				float value;
-				fl << &line[i];
+				std::string key;
+				for (size_t j = 0; j < i; j++)
+					key += line[j];
+				key += '\0';
+				fl << &line[i + 1];
 				fl >> value;
+				std::cout << key << value << std::endl;
 				map.insert(std::pair<std::string, float>(key, value));
 			}
 			else {
-				std::cout << "Error: bad input => " << line;
+				std::cout << "Error: bad input => " << line << std::endl;
 				continue ;
 			}
-    	}
-		for(std::map<std::string, float>::const_iterator it = map.begin(); it != map.end(); ++it)
-			std::cout << it->first << " " << it->second << std::endl;
+		}
+		// for(std::map<std::string, float>::const_iterator it = map.begin(); it != map.end(); it++)
+		// 	std::cout << it->first << "| |" << it->second << std::endl;
 	}
 }
